4c8016d41dde5f7539f3397f88e2e03d12009e48
Om Budhiraja <ombudhiraja144@gmail.com>
2022-03-19 23:34:54 +0530

    added login btn in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { BsSearch, BsArrowLeft } from 'react-icons/bs';
+import { BiUserCircle } from 'react-icons/bi';
 import { RiVideoUploadFill, RiVideoUploadLine } from 'react-icons/ri';
 import { ReactComponent as Logo } from './../assets/logo.svg';
 
@@ -15,8 +16,11 @@ export default function Navbar() {
     console.log(search);
     // vall api
   };
+
+  const user = null;
+
   return (
-    <div className="sticky top-0">
+    <div className="sticky top-0 z-10">
       <nav className="flex justify-between gap-8 items-center bg-zinc-800 bg-opacity-95 px-4 sm:px-12 py-3 border-b border-zinc-500 min-h-[69px]">
         {showSearchBarOnFullNav ? (
           <>
@@ -48,30 +52,45 @@ export default function Navbar() {
               >
                 <BsSearch className="h-5 w-5 sm:h-6 sm:w-6" color="white" />
               </div>
-              <NavLink
-                to={'/upload'}
-                className={({ isActive }) => {
-                  setIsUploadLinkActive(isActive);
-                  return '';
-                }}
-              >
-                {isUploadLinkActive ? (
-                  <RiVideoUploadFill
-                    className="h-6 w-6 sm:h-7 sm:w-7"
-                    color="white"
-                  />
-                ) : (
-                  <RiVideoUploadLine
-                    className="h-6 w-6 sm:h-7 sm:w-7"
-                    color="white"
-                  />
-                )}
-              </NavLink>
-              <img
-                src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200"
-                alt=""
-                className="rounded-full h-9 w-9 sm:h-11 sm:w-11"
-              />
+              {user ? (
+                <>
+                  <NavLink
+                    to={'/upload'}
+                    className={({ isActive }) => {
+                      setIsUploadLinkActive(isActive);
+                      return '';
+                    }}
+                  >
+                    {isUploadLinkActive ? (
+                      <RiVideoUploadFill
+                        className="h-6 w-6 sm:h-7 sm:w-7"
+                        color="white"
+                      />
+                    ) : (
+                      <RiVideoUploadLine
+                        className="h-6 w-6 sm:h-7 sm:w-7"
+                        color="white"
+                      />
+                    )}
+                  </NavLink>
+                  <div>
+                    <img
+                      src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200"
+                      alt=""
+                      className="rounded-full h-9 w-9 sm:h-11 sm:w-11"
+                    />
+                  </div>
+                </>
+              ) : (
+                <>
+                  <Link to={'/login'}>
+                    <button className="border border-blue-400 text-blue-400 flex items-center gap-2 py-2 px-4 uppercase font-semibold">
+                      <BiUserCircle size={26} />
+                      Login
+                    </button>
+                  </Link>
+                </>
+              )}
             </div>
           </>
         )}
